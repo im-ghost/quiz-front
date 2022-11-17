6e48d75c1568d5c32018799c8bf7f44c76c83bd4
Ghost <culestfrosh@gmail.com>
2022-11-17 01:30:27 -0800

    quiz page

--- src/features/quiz/quiz.jsx
+++ src/features/quiz/quiz.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './counterSlice'
+import { decrement, increment } from './quizSlice'
 
-export function Counter() {
-  const count = useSelector((state) => state.counter.value)
+export function Quiz() {
+  const count = useSelector((state) => state.quiz.value)
   const dispatch = useDispatch()
 
   return (
