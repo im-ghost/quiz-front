8565a8c60f53583478bae574a09fdf2d896c5c69
Ghost <culestfrosh@gmail.com>
2022-11-17 01:29:05 -0800

    users reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import  quizSlice  from '../features/quiz/quizSlice'
+import  userSlice  from '../features/user/userSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    quiz:quizSlice,
+    user:userSlice
+  },
 })
