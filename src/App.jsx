83fcb0453798e346ea7e6881364a807626dfe089
Ghost <culestfrosh@gmail.com>
2022-11-17 01:28:37 -0800

    added test component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Test from './pages/test'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+      <Test/>
     </div>
   )
 }
