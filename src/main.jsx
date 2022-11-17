60ebb0eeb0f225c46895d3f23c68ecff4a6cb8c4
Ghost <culestfrosh@gmail.com>
2022-11-17 01:29:39 -0800

    provider for redux

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,8 @@ import { store } from './app/store'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
      <Provider store={store}>
       <App/>
      </Provider>
-  </React.StrictMode>
+
 )
